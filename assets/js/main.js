f396f0197d52e0c0f82e5196d0beef4556ff6c57
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-22 23:14:56 +0100

    fix: adjust code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,32 +2,32 @@
 
 const arrayBrindisi = [
     {
-        immagine: "./assets/img/Brindisi_Landscape.jpg",
+        image: "Brindisi_Landscape.jpg",
         titolo: "Benvenuti a Brindisi!",
         descrizione: "Qui, potete ammirare alcune delle bellezze principali di Brindisi, una città ricca di storia, dal periodo romano passando per il Medioevo, fino a diventare capitale d'Italia",
     },
 
     {
-        immagine: "./assets/img/Brindisi_Colonne_romane.jpg",
+        image: "Brindisi_Colonne_romane.jpg",
         titolo: "Colonne romane sulla Scalinata Virgiliana",
         descrizione: "La tradizione sostiene che sono le colonne terminali della via Appia, che collegava Roma con Brindisi, durante la dominazione romana, ma alcuni studi studenti sostengono che risalgono al periodo bizantino. L'altra colonna si trova a Lecce, in piazza Sant'Oronzo.",
     },
 
     {
-        immagine: "./assets/img/Brindisi_Monumento_al_marinaio.jpg",
+        image: "Brindisi_Monumento_al_marinaio.jpg",
         titolo: "Monumento al marinaio",
         descrizione: "Il monumento ha la forma di timone, alta circa 53 metri. I lavori di costruzione durarono un anno e l'inaugurazione avvenne il 4 novembre 1933, alla presenza del re Vittorio Emanuele III.",
     },
 
     {
-        immagine: "./assets/img/Brindisi_Duomo.jpg",
+        image: "Brindisi_Duomo.jpg",
         titolo: "Duomo",
         descrizione: "La Cattedrale, di stile romanico, anche se la faccia risulta d'epoca barocca, in quanto ricostruita dopo il terremoto del 1743. Qui, furono celebrati due matrimoni reali: Ruggero, figlio di Tancredi di Sicilia e Isacco Angelo, principessa di Costantinopoli, nel 1191; Federico II di Svevia con Isabella di Brienne, nel 1225",
     },
 
     {
-        immagine: "./assets/img/Brindisi_Tempio_di_San Giovanni_al_Sepolcro.jpg",
-        titolo: "Tempio di San Giovanni",
+        image: "Brindisi_Tempio_di_San Giovanni_al_Sepolcro.jpg",
+        titolo: "Tempio di San Giovanni al Sepolcro",
         descrizione: "Uno dei monumenti più affascinanti di Brindisi, per via del suo portale, degli affreschi e delle tracce dei pellegini che si possono notare al suo interno",
     },
 ]
@@ -45,7 +45,7 @@ infoText.innerHTML = arrayBrindisi[0].descrizione;
 
 for (i = 0; i < arrayBrindisi.length; i++) {
   const singleImg = document.createElement("img");
-  singleImg.src = `./assets/img/${arrayBrindisi[i].immagine}`;
+  singleImg.src = `./assets/img/${arrayBrindisi[i].image}`;
   singleImg.classList.add("left-slider-img");
 
   if (i === 0) {
@@ -59,7 +59,7 @@ for (i = 0; i < arrayBrindisi.length; i++) {
 
 for (i = 0; i < arrayBrindisi.length; i++) {
   const singleImg = document.createElement("img");
-  singleImg.src = `./assets/img/${arrayBrindisi[i].immagine}`;
+  singleImg.src = `./assets/img/${arrayBrindisi[i].image}`;
   singleImg.classList.add("right-slider-img");
 
   if (i === 0) {
