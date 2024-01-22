5080c9a6408826f240865944edb102ac513b8b4c
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-22 16:15:49 +0100

    feat: add array objects

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// Array di oggetti con la foto e descrizione della foto
+
+const brindisiArr = [
+    {
+        image: "Brindisi_Landscape.jpg",
+        titolo: "Benvenuti a Brindisi!",
+        descrizione: "Qui, potete ammirare alcune delle bellezze principali di Brindisi, una città ricca di storia, dal periodo romano passando per il Medioevo, fino a diventare capitale d'Italia",
+    },
+
+    {
+        image: "Brindisi_Colonne_romane.jpg",
+        titolo: "Colonne romane sulla Scalinata Virgiliana",
+        descrizione: "La tradizione sostiene che sono le colonne terminali della via Appia, che collegava Roma con Brindisi, durante la dominazione romana, ma alcuni studi studenti sostengono che risalgono al periodo bizantino. L'altra colonna si trova a Lecce, in piazza Sant'Oronzo.",
+    },
+
+    {
+        image: "Brindisi_Monumento_al_marinaio.jpg",
+        titolo: "Monumeto al marinaio",
+        descrizione: "Il monumento ha la forma di timone, alta circa 53 metri. I lavori di costruzione durarono un anno e l'inaugurazione avvenne il 4 novembre 1933, alla presenza del re Vittorio Emanuele III,",
+    },
+
+    {
+        image: "Brindisi_Duomo.jpg",
+        titolo: "Duomo",
+        descrizione: "La Cattedrale, di stile romanico, anche se la faccia risulta d'epoca barocca, in quanto ricostruita dopo il terremoto del 1743. Qui, furono celebrati due matrimoni reali: Ruggero, figlio di Tancredi di Sicilia e Isacco Angelo, principessa di Costantinopoli, nel 1191; Federico II di Svevia con Isabella di Brienne, nel 1225",
+    },
+
+    {
+        image: "Brindisi_Tempio_di_San Giovanni_al_Sepolcro.jpg",
+        titolo: "Tempio di San Giovanni",
+        descrizione: "Uno dei monumenti più affascinanti di Brindisi, per via del suo portale, degli affreschi e delle tracce dei pellegini che si possono notare al suo interno",
+    },
+
+    {
+        image: "Brindisi_Scavi.jpg",
+        titolo: "Scavi di San Pietro degli Schiavoni",
+        descrizione: "Gli scavi di epoca romana, rinvenuti, in occasione dei lavori del nuovo teatro Giuseppe Verdi, nel 1964",
+    },
+]
