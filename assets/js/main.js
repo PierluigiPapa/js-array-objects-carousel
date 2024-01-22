796c39d25b390b2eb5b01063d379042495042e3f
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-22 22:56:36 +0100

    fix: adjust code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,84 +2,117 @@
 
 const arrayBrindisi = [
     {
-        image: "Brindisi_Landscape.jpg",
+        immagine: "./assets/img/Brindisi_Landscape.jpg",
         titolo: "Benvenuti a Brindisi!",
         descrizione: "Qui, potete ammirare alcune delle bellezze principali di Brindisi, una città ricca di storia, dal periodo romano passando per il Medioevo, fino a diventare capitale d'Italia",
     },
 
     {
-        image: "Brindisi_Colonne_romane.jpg",
+        immagine: "./assets/img/Brindisi_Colonne_romane.jpg",
         titolo: "Colonne romane sulla Scalinata Virgiliana",
         descrizione: "La tradizione sostiene che sono le colonne terminali della via Appia, che collegava Roma con Brindisi, durante la dominazione romana, ma alcuni studi studenti sostengono che risalgono al periodo bizantino. L'altra colonna si trova a Lecce, in piazza Sant'Oronzo.",
     },
 
     {
-        image: "Brindisi_Monumento_al_marinaio.jpg",
-        titolo: "Monumeto al marinaio",
+        immagine: "./assets/img/Brindisi_Monumento_al_marinaio.jpg",
+        titolo: "Monumento al marinaio",
         descrizione: "Il monumento ha la forma di timone, alta circa 53 metri. I lavori di costruzione durarono un anno e l'inaugurazione avvenne il 4 novembre 1933, alla presenza del re Vittorio Emanuele III.",
     },
 
     {
-        image: "Brindisi_Duomo.jpg",
+        immagine: "./assets/img/Brindisi_Duomo.jpg",
         titolo: "Duomo",
         descrizione: "La Cattedrale, di stile romanico, anche se la faccia risulta d'epoca barocca, in quanto ricostruita dopo il terremoto del 1743. Qui, furono celebrati due matrimoni reali: Ruggero, figlio di Tancredi di Sicilia e Isacco Angelo, principessa di Costantinopoli, nel 1191; Federico II di Svevia con Isabella di Brienne, nel 1225",
     },
 
     {
-        image: "Brindisi_Tempio_di_San Giovanni_al_Sepolcro.jpg",
+        immagine: "./assets/img/Brindisi_Tempio_di_San Giovanni_al_Sepolcro.jpg",
         titolo: "Tempio di San Giovanni",
         descrizione: "Uno dei monumenti più affascinanti di Brindisi, per via del suo portale, degli affreschi e delle tracce dei pellegini che si possono notare al suo interno",
-    },
-
-    {
-        image: "Brindisi_Scavi.jpg",
-        titolo: "Scavi di San Pietro degli Schiavoni",
-        descrizione: "Gli scavi di epoca romana, rinvenuti, in occasione dei lavori del nuovo teatro Giuseppe Verdi, nel 1964",
     },
 ]
 
 // console.log(arrayBrindisi)
 
-let currentImage = 0;
-let intervalId;
-const immaginiBrindisi = arrayBrindisi.length;
+const leftSlider = document.getElementById("left-slider");
+const rightSlider = document.getElementById("right-slider");
+const infoTitle = document.getElementById("info-title");
+const infoText = document.getElementById("info-text");
+infoTitle.innerHTML = arrayBrindisi[0].titolo;
+infoText.innerHTML = arrayBrindisi[0].descrizione;
 
-const carouselImageHtml = document.getElementById('image');
-const carouselTitleHtml = document.getElementById('title');
-const carouselTextHtml = document.getElementById('description');
+// slider left
 
-const prevHtml = document.getElementById('preview');
-const nextHtml = document.getElementById('next');
-const playHtml = document.getElementById('play');
-const stopHtml = document.getElementById('stop');
+for (i = 0; i < arrayBrindisi.length; i++) {
+  const singleImg = document.createElement("img");
+  singleImg.src = `./assets/img/${arrayBrindisi[i].immagine}`;
+  singleImg.classList.add("left-slider-img");
 
-const startCarousel = () => {
-    intervalId = setInterval(() => {
-        currentImage = (currentImage + 1) % immaginiBrindisi;
-        showImage(currentImage);
-    }, 3000);
+  if (i === 0) {
+    singleImg.classList.add("active");
+  }
+
+  leftSlider.append(singleImg);
 }
 
-const stopCarousel = () => {
-    clearInterval(intervalId);
+// slider right
+
+for (i = 0; i < arrayBrindisi.length; i++) {
+  const singleImg = document.createElement("img");
+  singleImg.src = `./assets/img/${arrayBrindisi[i].immagine}`;
+  singleImg.classList.add("right-slider-img");
+
+  if (i === 0) {
+    singleImg.classList.add("selected");
+  }
+  rightSlider.append(singleImg);
 }
 
-playHtml.addEventListener('click', startCarousel);
-stopHtml.addEventListener('click', stopCarousel);
+let currentImg = 0;
+const listLeftImg = document.querySelectorAll(".left-slider-img");
+const listRightImg = document.querySelectorAll(".right-slider-img");
 
-const showImage = (index) => {
-    const { image, titolo, descrizione } = arrayBrindisi[index];
-    carouselImageHtml.src = image;
-    carouselTitleHtml.textContent = titolo;
-    carouselTextHtml.textContent = descrizione;
+const upButton = document.getElementById("up-button");
+const downButton = document.getElementById("down-button");
+
+function next() {
+  listLeftImg[currentImg].classList.remove("active");
+  listRightImg[currentImg].classList.remove("selected");
+
+  currentImg++;
+
+  if (currentImg === arrayBrindisi.length) {
+    currentImg = 0;
+  }
+
+  listLeftImg[currentImg].classList.add("active");
+  listRightImg[currentImg].classList.add("selected");
+  infoTitle.innerHTML = arrayBrindisi[currentImg].titolo;
+  infoText.innerHTML = arrayBrindisi[currentImg].descrizione;
 }
 
-prevHtml.addEventListener('click', () => {
-    currentImage = (currentImage- 1 + immaginiBrindisi) % immaginiBrindisi;
-    showImage(currentImage);
-})
+function previous() {
+  listLeftImg[currentImg].classList.remove("active");
+  listRightImg[currentImg].classList.remove("selected");
 
-nextHtml.addEventListener('click', () => {
-    currentImage = (currentImage + 1) % immaginiBrindisi;
-    showImage(currentImage);
-})
+  if (currentImg === 0) {
+    currentImg = arrayBrindisi.length;
+  }
+
+  currentImg--;
+
+  listLeftImg[currentImg].classList.add("active");
+  listRightImg[currentImg].classList.add("selected");
+  infoTitle.innerHTML = arrayBrindisi[currentImg].titolo;
+  infoText.innerHTML = arrayBrindisi[currentImg].descrizione;
+}
+
+downButton.addEventListener("click", function () {
+  next();
+});
+
+upButton.addEventListener("click", function () {
+  previous();
+});
+
+const autoSlide = setInterval(next, 3000);
